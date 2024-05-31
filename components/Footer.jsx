2c85aa2c2c0cc8ece7b91d53e8cac518c2b2dc8c
leoleoyuuki <rm550373@fiap.com.br>
2024-05-31 19:07:47 -0300

    Add Footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+const Footer = ({navigation}) => {
+  return (
+    <View style={styles.footer}>
+      <TouchableOpacity onPress={()=> navigation.navigate('Home')}>
+          <Image source={require("../assets/Logo.png")} style={styles.img}  />
+      </TouchableOpacity>
+      <Text style={styles.footerText}>
+        © {new Date().getFullYear()} EcoWater Embarcações. Todos os direitos
+        reservados.
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    width: "100%",
+    backgroundColor: "#0d0d0d",
+    paddingVertical: 18,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  img: {
+    width: 150,
+    height: 25,
+  },
+  footerText: {
+    paddingTop: 15,
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "semibold",
+    textAlign: "center",
+  },
+});
+
+export default Footer;
